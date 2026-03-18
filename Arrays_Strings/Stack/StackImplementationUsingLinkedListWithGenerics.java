0a9906894457eb8b5d02bd701eb093c2158f244b
sonam sharma <sonamsh90002gmail.com>
2026-03-18 10:38:57 +0530

    [sonam]-added Stack Implementation Using Linked List With Generics Solution

---
+++ Arrays_Strings/Stack/StackImplementationUsingLinkedListWithGenerics.java
@@ -0,0 +1,45 @@
+class Stack<T> {
+
+    class Node {
+        T data;
+        Node next;
+
+        Node(T data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    private Node top;
+
+    Stack() {
+        top = null;
+    }
+
+    // Push
+    void push(T value) {
+        Node newNode = new Node(value);
+        newNode.next = top;
+        top = newNode;
+    }
+
+    // Pop
+    T pop() {
+        if (top == null) {
+            System.out.println("Stack Underflow");
+            return null;
+        }
+        T val = top.data;
+        top = top.next;
+        return val;
+    }
+
+    // Peek
+    T peek() {
+        if (top == null) {
+            System.out.println("Stack is empty");
+            return null;
+        }
+        return top.data;
+    }
+}
